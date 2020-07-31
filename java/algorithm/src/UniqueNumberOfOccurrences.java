81e912044ed0a247d75f0805098171fb764ff7b1
skycoop <www.skycoop@163.com>
2020-07-31 10:20:23 +0800

    UniqueNumberOfOccurrences

---
+++ java/algorithm/src/UniqueNumberOfOccurrences.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            int count = map.getOrDefault(i, 0);
+            map.put(i, count + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+        set.addAll(map.values());
+        return set.size() == 1;
+    }
+}
