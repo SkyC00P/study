019ea858cdcac17a2fb273adf32821d269de87d6
Skycoop <skycoop@163.com>
2020-08-01 18:59:59 +0800

    FindWinnerOnATicTacToeGame

--- java/algorithm/src/UniqueNumberOfOccurrences.java
+++ java/algorithm/src/UniqueNumberOfOccurrences.java
@@ -4,7 +4,11 @@ import java.util.Map;
 import java.util.Set;
 
 public class UniqueNumberOfOccurrences {
-    
+    /**
+     * Runtime: 2 ms, faster than 59.15% of Java online submissions for Unique
+     * Number of Occurrences. Memory Usage: 39.8 MB, less than 6.14% of Java online
+     * submissions for Unique Number of Occurrences.
+     */
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i : arr) {
