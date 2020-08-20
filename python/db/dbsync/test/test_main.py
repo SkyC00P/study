e8054d6c7667c603020880edb980b94e36a556db
Skycoop <skycoop@163.com>
2020-08-20 18:46:34 +0800

    python problem record

--- python/db/dbsync/test/test_main.py
+++ python/db/dbsync/test/test_main.py
@@ -4,4 +4,4 @@ import unittest   # The test framework
 
 class Test_TestIncrementDecrement(unittest.TestCase):
     def test_increment(self):
-        self.assertEqual(tools.add(3), 4)
+        self.assertIsNotNone(tools.OracleDB(None))
