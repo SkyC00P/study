ded54fd546ddfef4e0f6717899413ffa1fd13d93
skycoop <www.skycoop@163.com>
2020-08-31 11:41:22 +0800

    done: XOR_Operation_in_an_Array RunningSum_of_1dArray FinalPricesWith_aSpecialDiscount_in_aShop undone: MakeTwoArraysEqualByReversingSubArrays

---
+++ java/algorithm/src/RunningSum_of_1dArray.java
@@ -0,0 +1,15 @@
+public class RunningSum_of_1dArray {
+    /**
+     * Runtime: 0 ms, faster than 100.00% of Java online submissions for Running Sum
+     * of 1d Array. Memory Usage: 40.4 MB, less than 30.44% of Java online
+     * submissions for Running Sum of 1d Array.
+     */
+    public int[] runningSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum = nums[i] + sum;
+            nums[i] = sum;
+        }
+        return nums;
+    }
+}
---
+++ java/algorithm/src/todo/MakeTwoArraysEqualByReversingSubArrays.java
@@ -0,0 +1,6 @@
+package todo;
+public class MakeTwoArraysEqualByReversingSubArrays {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        return true;
+    }
+}
