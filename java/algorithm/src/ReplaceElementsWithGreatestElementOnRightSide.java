0766af12a8ed3e92f157201ad5aac3a76aa27c3a
Skycoop <skycoop@163.com>
2020-09-04 14:20:24 +0800

    FindNumbersWithEvenNumberOfDigits ReplaceElementsWithGreatestElementOnRightSide

---
+++ java/algorithm/src/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,20 @@
+public class ReplaceElementsWithGreatestElementOnRightSide {
+    /**
+     * Runtime: 1 ms, faster than 99.75% of Java online submissions for Replace
+     * Elements with Greatest Element on Right Side. Memory Usage: 40.5 MB, less
+     * than 96.20% of Java online submissions for Replace Elements with Greatest
+     * Element on Right Side.
+     */
+    public int[] replaceElements(int[] arr) {
+        int max = arr[arr.length - 1];
+        arr[arr.length - 1] = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            int cur = arr[i];
+            arr[i] = max;
+            if (cur > max) {
+                max = cur;
+            }
+        }
+        return arr;
+    }
+}
